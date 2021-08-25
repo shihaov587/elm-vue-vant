58e5a5a538b4e06cc7276726138f22ad58345ef1
tomato <851070169@qq.com>
2021-08-25 17:57:40 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import less from 'less'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+import Vant from 'vant';
+import 'vant/lib/index.css';
 import router from './router'
 import store from './store'
+import './config/rem'
+import './assets/css/common.css'
 require("./mock/mock.js")
 Vue.config.productionTip = false
-Vue.use(ElementUI);
+// Vue.use(ElementUI);
 Vue.use(less)
+Vue.use(Vant);
 
 new Vue({
   router,
---
+++ src/views/home/home.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="home">
+    <header>
+      <van-search
+        v-model="searchValue"
+        show-action
+        placeholder="请输入搜索关键词"
+        @focus="onSearchFocus"
+        @search="onSearch"
+      >
+        <template #action>
+          <div @click="onSearch">搜索</div>
+        </template>
+      </van-search>
+    </header>
+    <nav>
+      <van-swipe :autoplay="3000">
+        <van-swipe-item v-for="(image, index) in images" :key="index">
+          <img :src="image" class="img_fill"/>
+        </van-swipe-item>
+      </van-swipe>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchValue: "",
+      images: [
+        'https://img01.yzcdn.cn/vant/apple-1.jpg',
+        'https://img01.yzcdn.cn/vant/apple-2.jpg',
+      ],
+    };
+  },
+  methods: {
+    // 焦点搜索框弹出输入框
+    onSearchFocus() {},
+  },
+};
+</script>
+
+<style lang="less" scoped>
+#home {
+  header {
+    width: 100%;
+    height: 1.95rem;
+    position: fixed;
+    top: 0;
+    z-index: 100;
+    padding: 0 1rem;
+  }
+  nav {
+    width: 100%;
+    height: 10rem;
+    padding-top: 2.5rem;
+    .van-swipe {
+        width: 100%;
+        height: 100%;
+    }
+  }
+}
+</style>
